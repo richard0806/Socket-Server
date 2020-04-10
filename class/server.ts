e48048be106f1ada09cf9340c14128560bbcf5df
richard0806 <richardvalenzuela2009@hotmail.com>
2020-04-10 10:24:16 -0400

    Server conectado a frondEnd para recibir y enviar mensajes

--- class/server.ts
+++ class/server.ts
@@ -1,16 +1,43 @@
 import express from "express";
 import { SERVER_PORT } from "../global/environment";
+import socketIO from "socket.io";
+import http from "http";
+
+import * as socket from "../sockets/sockets";
 
 export default class Server {
+  private static _intance: Server;
   public app: express.Application;
   public port: number;
 
-  constructor() {
+  public io: socketIO.Server;
+  private httpServer: http.Server;
+
+  private constructor() {
     this.app = express();
     this.port = SERVER_PORT;
+    this.httpServer = new http.Server(this.app);
+    this.io = socketIO(this.httpServer);
+    this.escucharSockets();
+  }
+
+  public static get instance() {
+    return this._intance || (this._intance = new this());
+  }
+
+  private escucharSockets() {
+    console.log("Escuchando conexioones - sockets");
+    this.io.on("connection", (cliente) => {
+      console.log("Cliente conectado");
+      // Desconectar
+      socket.desconectar(cliente);
+      // Mensajes
+      socket.mensaje(cliente, this.io);
+    });
   }
 
   start(callback: Function) {
-    this.app.listen(this.port, callback);
+    //this.app.listen(this.port, callback);
+    this.httpServer.listen(this.port, callback);
   }
 }
